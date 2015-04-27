2df806ca14db3fb73c377ff91ec3ace46c2c2de0
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-04-28 01:14:20 +0200

    implemented all four methods respectively

--- assignment.py
+++ assignment.py
@@ -1,13 +1,31 @@
-def onemax(bitstring):
-        return bitstring.count('1')
+def onemax(bs):
+        return bs.count('1')
 
-def leadingones(bitstring):
+def leadingones(bs):
         count = 0
-        for c in bitstring:
-                if c != '1':
+        for c in bs:
+                if (c != '1'):
                         break
                 count += 1
         return count
 
+def jumpk(bs, k):
+        n = len(bs)
+        if (onemax(bs) < (n-k)):
+                return onemax(bs)
+        elif ((n-k) <= onemax(bs)) and (onemax(bs) < n):
+                return n - k
+        elif (onemax(bs) == n):
+                return n
+        else:
+                raise "WAT"
 
-print(leadingones('111100011'))
+def binval(bs):
+        return int(bs, 2)
+
+bitstring = '1100010101011001011110'
+
+print("OneMax: ", onemax(bitstring))
+print("LeadingOnes: ", leadingones(bitstring))
+print("Jump K: ", jumpk(bitstring, 4))
+print("BinVal: ", binval(bitstring))
