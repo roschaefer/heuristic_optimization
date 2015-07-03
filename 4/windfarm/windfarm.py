1bb4aa384121c0e509a69cebee022ea7f693c433
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-07-03 19:58:23 +0200

    current state of development again

--- 4/windfarm/windfarm.py
+++ 4/windfarm/windfarm.py
@@ -4,19 +4,11 @@ from IPython import embed
 import subprocess
 from random import randint
 
-#def f(x, y):
-    #return -x**2 - y**2
-
-#optimal_pars, details, _ = optunity.maximize(f, num_evals=100, x=[-5, 5], y=[-5, 5])
-
 def points():
     return  [(randint(0,10000),randint(0,10000)) for _ in range(100)]
 
 def points_formatted(points):
     return '\n'.join([' '.join(map(str,p)) for p in points])
-
-
-
 
 def print_points(points):
     with open('optimized.dat', 'w') as the_file:
@@ -25,9 +17,41 @@ def print_points(points):
             the_file.write(points_formatted(points))
             the_file.write('\n')
 
-print_points(points())
+def in_F1(point):
+    x,y = point
+    return (0<=x<4000) and (0<=y<10000)
 
-ps = subprocess.Popen(("cat", "optimized.dat"), stdout=subprocess.PIPE)
-cost = subprocess.check_output(("./fcost"), stdin=ps.stdout)
+def in_F2(point):
+    x,y = point
+    return (6000<=x<10000) and (0<=y<10000)
+
+def in_F3(point):
+    x,y = point
+    return (0<=x<10000) and (2000<=y<8000)
+
+def bound_box_constraint(points):
+    for p in points:
+        if ( not (in_F1(p) or in_F2(p) or in_F3(p))):
+                return False
+        return True
+
+def security_distance_constraint(points):
+    for x1,y1 in points:
+        for x2,y2 in points:
+            if ((x1-x2)**2 + (y1-y2)**2) >= 308:
+                return False
+    return True
+
+def good_points():
+    pos = points()
+    while not (bound_box_constraint(pos) and security_distance_constraint(pos)):
+        pos = points()
+    return pos
+
+def costs():
+    print_points(points())
+    ps = subprocess.Popen(("cat", "optimized.dat"), stdout=subprocess.PIPE)
+    costs = subprocess.check_output(("./fcost"), stdin=ps.stdout)
+    return float(costs)
 
 embed()
